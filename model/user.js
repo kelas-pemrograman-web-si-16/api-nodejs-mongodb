9618afa528ed27ea419f32bbdba36e3fe43c42d9
dwiramadhan <dwi.13421054@student.ubl.ac.id>
2019-05-15 08:49:29 +0700

    api tokobuku

---
+++ model/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    username 			: {type: String, unique: true},
+    namalengkap 	    : String,
+    email			    : {type: String, unique: true},
+    hashed_password	    : String,
+    created_at		    : String,
+    temp_password	    : String,
+    temp_password_time  : String,
+    api_token           : String
+});
+module.exports = mongoose.model('user', userSchema);
